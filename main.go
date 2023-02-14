b81769f2515991bcf8bdfebd137daedfcb4d7773
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2023-02-14 04:24:45 +0100

    load keys and decode to type rsa keys

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	// "crypto/rsa"
+	"crypto/rsa"
 	"crypto/x509"
 	"encoding/hex"
 	"encoding/pem"
@@ -10,23 +10,28 @@ import (
 
 	"github.com/galadd/private-network/network"
 	"gopkg.in/AlecAivazis/survey.v1"
+	"github.com/joho/godotenv"
 
 	auth "github.com/galadd/private-network/authentication"
 )
 
 var (
-		hexEncodedPriv  string
-		hexEncodedPub   string
+	myAuthPrivateKey  *rsa.PrivateKey
+	respAuthPublicKey *rsa.PublicKey
 
-		err 			error
+	hexEncodedPriv    string
+	hexEncodedPub     string
+
+	err 			  error
 )
 
 func main() {
+	fmt.Println("Start the program if you have a keypair and responder's public key in .env file")
 	usage := ""
 	prompt := &survey.Select{
 		Message: "Select",
 		Options: []string{
-			"Create a new keypair",
+			"Generate a new keypair",
 			"Use an existing keypair",
 			"Exit",
 		},
@@ -42,8 +47,58 @@ func main() {
 		}
 
 		write(hexEncodedPriv, hexEncodedPub)
+		respHexPub := os.Getenv("RESPONDER_PUB_KEY")
+		respAuthPublicKey, err = decodeHexPub(respHexPub)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		myHexPriv := os.Getenv("HEX_ENCODED_PRIVATE")
+		myAuthPrivateKey, err = decodeHexPriv(myHexPriv)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		start := ""
+		prompt = &survey.Select{
+			Message: "",
+			Options: []string{
+				"Start",
+				"Exit",
+			},
+		}
+		survey.AskOne(prompt, &start, nil)
+
+		switch start {
+		case "Start":
+			operation()
+		case "Exit":
+			os.Exit(0)
+		}
 
 	case "Use an existing keypair":
+		err = godotenv.Load(".env")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		myHexPriv := os.Getenv("HEX_ENCODED_PRIVATE")
+		myAuthPrivateKey, err = decodeHexPriv(myHexPriv)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		respHexPub := os.Getenv("RESPONDER_PUB_KEY")
+		respAuthPublicKey, err = decodeHexPub(respHexPub)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		operation()
 		
 
 	case "Exit":
@@ -64,9 +119,9 @@ func operation() {
 	survey.AskOne(prompt, &choice, nil)
 	switch choice {
 	case "initiator":
-		network.ServerMain()
+		network.ServerMain(myAuthPrivateKey, respAuthPublicKey)
 	case "responder":
-		network.ClientMain()
+		network.ClientMain(myAuthPrivateKey, respAuthPublicKey)
 	case "exit":
 		os.Exit(0)
 	}
@@ -104,6 +159,48 @@ func generate() (string, string, error) {
 	return hexEncodedPriv, hexEncodedPub, nil
 }
 
+func decodeHexPriv(hexEncodedPriv string) (*rsa.PrivateKey, error) {
+	// convert hex encoded string to byte slice
+	decoded, err := hex.DecodeString(hexEncodedPriv)
+	if err != nil {
+		return nil, err
+	}
+
+	// load private key
+	block, _ := pem.Decode(decoded)
+	if block == nil {
+		return nil, fmt.Errorf("failed to parse PEM block containing the private key")
+	}
+
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return privateKey, nil
+}
+
+func decodeHexPub(hexEncodedPub string) (*rsa.PublicKey, error) {
+	// convert hex encoded string to byte slice
+	decoded, err := hex.DecodeString(hexEncodedPub)
+	if err != nil {
+		return nil, err
+	}
+
+	// load public key
+	block, _ := pem.Decode(decoded)
+	if block == nil {
+		return nil, fmt.Errorf("failed to parse PEM block containing the public key")
+	}
+
+	publicKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return publicKey.(*rsa.PublicKey), nil
+}
+
 func write(hexEncoded, hexEncodedPub string) {
 	file, err := os.Create(".env")
 	if err != nil {
@@ -113,8 +210,8 @@ func write(hexEncoded, hexEncodedPub string) {
 	defer file.Close()
 
 	envVars := map[string]string{
-		"hexEncoded-Private": hexEncoded,
-		"hexEncoded-Public":   hexEncodedPub,
+		"HEX_ENCODED_PRIVATE": hexEncoded,
+		"HEX_ENCODED_PUBLIC":   hexEncodedPub,
 	}
 
 	for key, value := range envVars {
